37911ebc5d8ef56e9485ffe62c0ebe5b9a4df444
john katua <johnkatua99@gmail.com>
2022-08-01 11:50:37 +0300

    added timestamp to users

--- server/resources/user/user.model.js
+++ server/resources/user/user.model.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      default: 'student',
+      enum: ["student", "admin"]
+    },
+    accessToken: {
+      type: String
+    }
   },
-  password: {
-    type: String,
-    required: true
-  },
-  role: {
-    type: String,
-    default: 'student',
-    enum: ["student", "admin"]
-  },
-  accessToken: {
-    type: String
-  }
-});
+  { timestamps: true }
+);
 
 const user = mongoose.model('user', userSchema);
 
