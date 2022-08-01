52c364483c0bec37fe3739a3a78820fab0a3e757
john katua <johnkatua99@gmail.com>
2022-08-01 12:22:26 +0300

    added a faculty folder

---
+++ server/resources/faculty/faculty.controllers.js
@@ -0,0 +1,13 @@
+const Faculty = require('./faculty.model');
+
+exports.createFaculty = async (req, res) => {
+
+}
+
+exports.getFaculty = async (req, res) => {
+
+}
+
+exports.getAllFaculties = async (req, res) => {
+  
+}
---
+++ server/resources/faculty/faculty.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const facultySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    acronym: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+const faculty = mongoose.model('faculty', facultySchema);
+
+module.exports = faculty;
