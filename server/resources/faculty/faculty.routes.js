0fd7b7e8d3a4a6019a5cfc275185bbca54ef382c
john katua <johnkatua99@gmail.com>
2022-08-02 11:07:25 +0300

    only authorized user and should be an admin so as to add faculty

--- server/resources/faculty/faculty.routes.js
+++ server/resources/faculty/faculty.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const facultyController = require('./faculty.controllers');
+const { authorize, isAdmin } = require('../../middleware/auth');
 
-router.post('/createFaculty', facultyController.createFaculty);
+router.post('/createFaculty', authorize, isAdmin, facultyController.createFaculty);
 
 module.exports = router;
