c6e727152e03e926345d03d25912b259ae3fdf26
john katua <johnkatua99@gmail.com>
2022-08-02 15:47:13 +0300

    only admins can create courses

--- server/resources/course/course.routes.js
+++ server/resources/course/course.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const courseController = require('./course.controllers');
+const { authorize, isAdmin } = require('../../middleware/auth');
 
-router.post('/createCourse', courseController.createCourse);
+router.post('/createCourse', authorize, isAdmin, courseController.createCourse);
 
 module.exports = router;
