b35513d83d6a45dc9f330519d5196b0852d64228
john katua <johnkatua99@gmail.com>
2022-08-02 13:48:38 +0300

    added course resource and its model

---
+++ server/resources/course/course.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    courseCode: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "degree",
+      enum: ["degree", "diploma"]
+    },
+    facultyId: {
+      type: mongoose.SchemaType.ObjectId,
+      ref: 'faculty',
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+const course = mongoose.model('course', courseSchema);
+
+module.exports = course;
