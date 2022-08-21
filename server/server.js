aebb7ea790c8bd0030e25f54dc9d61bcba92f796
john katua <johnkatua99@gmail.com>
2022-08-21 12:42:17 +0300

    added cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose  = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const userRoutes = require('./resources/user/user.routes');
 const facultyRoutes = require('./resources/faculty/faculty.routes');
@@ -13,6 +14,7 @@ require('dotenv').config({
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 4000;
 
