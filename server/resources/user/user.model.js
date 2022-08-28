51dd7b11a1ea3ec467fed19342a23a8ce9c950db
john katua <johnkatua99@gmail.com>
2022-08-28 15:05:01 +0300

    added a last name field in the user schema

--- server/resources/user/user.model.js
+++ server/resources/user/user.model.js
@@ -7,6 +7,9 @@ const userSchema = new Schema(
       type: String,
       required: true
     },
+    lastName: {
+      type: String,
+    },
     email: {
       type: String,
       required: true,
