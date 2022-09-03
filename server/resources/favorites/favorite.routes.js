213cf920960a930abd8479055e332472847555f8
john katua <johnkatua99@gmail.com>
2022-09-03 16:45:18 +0300

    added router to add papers to the favorite list

--- server/resources/favorites/favorite.routes.js
+++ server/resources/favorites/favorite.routes.js
@@ -1,1 +1,8 @@
-console.log('fav');
+const express = require('express');
+const routes = express().Router();
+
+const favoriteController = require('./favorite.contriollers');
+
+routes.post('/addFavPaper', favoriteController.createFavorite);
+
+module.exports = router;
