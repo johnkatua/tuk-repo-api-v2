4a4b18c6cd7297e4190f35b5c8f23ed3e48e4c33
john katua <johnkatua99@gmail.com>
2022-09-03 23:17:56 +0300

    added a route to fetch all fav papers

--- server/resources/favorites/favorite.routes.js
+++ server/resources/favorites/favorite.routes.js
@@ -6,5 +6,6 @@ const { authorize } = require('../../middleware/auth');
 const favoriteController = require('./favorite.contriollers');
 
 router.post('/addFavPaper', authorize, favoriteController.createFavorite);
+router.get('/getAllFav', favoriteController.getFavPapers);
 
 module.exports = router;
