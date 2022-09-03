6b17f60b0e86c9fbd0bede21a59bd9abe4367ced
john katua <johnkatua99@gmail.com>
2022-09-03 16:33:48 +0300

    add a controller to  create a new fav list

---
+++ server/resources/favorites/favorite.contriollers.js
@@ -0,0 +1,31 @@
+const Favorite = require('./favorite.model')
+
+exports.createFavorite = async (req, res) => {
+  const { paperId, paperName } = req.body;
+  const userId = req.user;
+  try {
+    const myPapers = await Favorite.findOne({ userId });
+    if (myPapers) {
+      const paperIdx = myPapers.papers.findIndex(p => p.paperId === paperId);
+      if (paperIdx < -1) {
+        myPapers.papers.push({ paperId, paperName });
+      }
+      myPapers = await myPapers.save();
+      return res.status(201).json({
+        msg: 'Paper add successfully to your favorite list'
+      })
+    } else {
+      const newList = await Favorite.create({
+        userId,
+        papers: [{ paperId, paperName }]
+      });
+      return res.status(201).json({
+        msg: 'Paper add successfully to your favorite list'
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      msg: 'Something went wrong'
+    })
+  }
+};
