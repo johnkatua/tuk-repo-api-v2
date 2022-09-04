537d532985cc82a49b7649420ebeed43eb5666fe
john katua <johnkatua99@gmail.com>
2022-09-04 14:56:06 +0300

    added route to delete paper from fav list

--- server/resources/favorites/favorite.routes.js
+++ server/resources/favorites/favorite.routes.js
@@ -7,5 +7,6 @@ const favoriteController = require('./favorite.contriollers');
 
 router.post('/addFavPaper', authorize, favoriteController.createFavorite);
 router.get('/getAllFav/:id', favoriteController.getFavPapers);
+router.delete('/deletePaper', authorize, favoriteController.deletePaper);
 
 module.exports = router;
