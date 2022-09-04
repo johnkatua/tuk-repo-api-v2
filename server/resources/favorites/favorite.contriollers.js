81523e08484c0b0016270fc331f4e800e60553b0
john katua <johnkatua99@gmail.com>
2022-09-04 10:40:04 +0300

    successfully display error message when the user id provide is not correct

--- server/resources/favorites/favorite.contriollers.js
+++ server/resources/favorites/favorite.contriollers.js
@@ -51,6 +51,12 @@ exports.getFavPapers = async (req, res) => {
         data: data
       })
     }).catch(err => {
+      if(err.kind === 'ObjectId') {
+        return res.status(404).json({
+          success: false,
+          msg: `User with an id of ${req.params.id} is not found`
+        })
+      }
       res.status(500).send({
         success: false,
         msg: err.message || 'Unable to fetch Papers'
