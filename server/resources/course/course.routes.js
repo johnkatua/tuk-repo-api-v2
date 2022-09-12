2300c59119ef7a1b76f0053605c52f9f3f0ee3bd
john katua <johnkatua99@gmail.com>
2022-09-12 08:20:01 +0300

    restricted fetching of all courses from the api

--- server/resources/course/course.routes.js
+++ server/resources/course/course.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const courseController = require('./course.controllers');
 const { authorize, isAdmin } = require('../../middleware/auth');
 
-router.get('/getAllCourses', courseController.getAllCourses);
+router.get('/getAllCourses', authorize, courseController.getAllCourses);
 router.get('/getCourse/:id', courseController.getCourse);
 router.put('/updateCourse/:id', authorize, isAdmin, courseController.updateCourse);
 router.post('/createCourse', authorize, isAdmin, courseController.createCourse);
