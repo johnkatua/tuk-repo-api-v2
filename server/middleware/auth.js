e947e1dc52c8901dd6874ebc0a63cdd8a4ceee0a
john katua <johnkatua99@gmail.com>
2022-09-19 13:16:56 +0300

    removed the console.log from the auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,6 @@ exports.authorize = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
     req.userData = { email: decoded.email,  userId: decoded.userId, role: decoded.role };
     next();
   } catch (error) {
