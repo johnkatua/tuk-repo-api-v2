33efa27e707d71c695790be386160862091c627c
john katua <johnkatua99@gmail.com>
2022-09-21 13:55:31 +0300

    only authorized users can get a single faculty

--- server/resources/faculty/faculty.routes.js
+++ server/resources/faculty/faculty.routes.js
@@ -5,7 +5,7 @@ const facultyController = require('./faculty.controllers');
 const { authorize, isAdmin } = require('../../middleware/auth');
 
 router.get('/getAllFaculties', authorize, facultyController.getAllFaculties);
-router.get('/getFaculty/:id',facultyController.getFaculty);
+router.get('/getFaculty/:id',authorize, facultyController.getFaculty);
 router.post('/createFaculty', authorize, isAdmin, facultyController.createFaculty);
 router.put('/updateFaculty/:id', authorize, isAdmin, facultyController.updateFaculty);
 router.delete('/deleteFaculty/:id', authorize, isAdmin, facultyController.deleteFaculty);
