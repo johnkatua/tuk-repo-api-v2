b5001f17973aa7fcb50b23c8d969be92facbb98f
john katua <johnkatua99@gmail.com>
2022-10-12 10:15:18 +0300

    removed unnecessary comments from the course schema

--- server/resources/course/course.model.js
+++ server/resources/course/course.model.js
@@ -35,32 +35,6 @@ courseSchema.pre('deleteOne', async function (next) {
   next();
 })
 
-// facultySchema.pre('deleteOne', async function (next) {
-//   const facultyId = this.getQuery()['_id'];
-//   await Course.deleteMany({ facultyId }).exec();
-//   await Paper.deleteMany({ facultyId }).exec();
-//   next()
-// })
-
 const course = mongoose.model('Course', courseSchema);
 
 module.exports = course;
-
-// courseSchema.pre('deleteOne', function (next) {
-//   let course = this;
-//   course.model('paper').deleteMany({ course: course._id }, function (err, res) {
-//     if (err) {
-//       res.status(500).json({
-//         msg: err
-//       })
-//       console.log(err);
-//       next(err)
-//     } else {
-//       res.status(200).json({
-//         msg: 'Delete successfully'
-//       })
-//       console.log('success');
-//       next()
-//     }
-//   });
-// });
