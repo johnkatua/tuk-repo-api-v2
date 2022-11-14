bcb224cb0929438d715236a8610b3ecce2acbda1
john katua <johnkatua99@gmail.com>
2022-11-14 12:10:36 +0300

    added a functionality to send email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,1 +1,14 @@
-console.log('working on email');
+const nodemailer = require('nodemailer');
+
+export const sendEmail = ({ from, to, subject, text }) => {
+  const client = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  const message = { from, to, subject, text };
+  return client.sendMail(message);
+}
